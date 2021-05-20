21d22b45b02c7e1e673f1fb946862fec5e048393
lauralani <dev@lauka.space>
2021-05-20 10:20:03 +0200

    added live api for testing

--- api/TextPost.cs
+++ api/TextPost.cs
@@ -8,6 +8,7 @@ using Microsoft.AspNetCore.Http;
 using Newtonsoft.Json;
 using Lauka.pi.Classes;
 using System.Collections.Generic;
+using RestSharp;
 
 namespace Lauka.pi
 {
@@ -19,6 +20,8 @@ namespace Lauka.pi
         {
             string request = await new StreamReader(req.Body).ReadToEndAsync();
 
+            int fontsize = int.Parse(Environment.GetEnvironmentVariable("APP_BACKEND_DEFAULT_FONTSIZE"));
+
             TextRequest text;
             try
             {
@@ -29,12 +32,29 @@ namespace Lauka.pi
                 return new BadRequestResult();
             }
 
-            return new ContentResult
+            Dictionary<string, Object> restbody = new Dictionary<string, Object>()
             {
-                Content = JsonConvert.SerializeObject( new Dictionary<string, string>() { {"status", "success" } } ),
-                ContentType = "application/json",
-                StatusCode = 200
+                {"text", text.Text},
+                {"fontsize", fontsize}
             };
+
+            var restclient = new RestClient("https://a-pi-backend.lauka.app/text");
+            restclient.Timeout = -1;
+            RestRequest restrequest = new RestRequest(Method.POST);
+            restrequest.AddHeader("x-api-key", Environment.GetEnvironmentVariable("APP_BACKEND_APIKEY"));
+            restrequest.AddHeader("Content-Type", "application/json");
+            restrequest.AddParameter("application/json", JsonConvert.SerializeObject(restbody), ParameterType.RequestBody);
+            IRestResponse restresponse = restclient.Execute(restrequest);
+
+            if (restresponse.IsSuccessful)
+                return new ContentResult
+                {
+                    Content = JsonConvert.SerializeObject(new Dictionary<string, string>() { { "status", "success" } }),
+                    ContentType = "application/json",
+                    StatusCode = 200
+                };
+            else
+                return new BadRequestObjectResult("something went wrong");
         }
     }
 }
